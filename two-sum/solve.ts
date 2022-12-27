9cf983e4720a0cde72877b0a66a6b7d7375a38ca
Jeel Patel <jeel.patel1111@gmail.com>
2022-12-27 23:58:38 +0800

    two-sum solved

---
+++ two-sum/solve.ts
@@ -0,0 +1,18 @@
+function twoSum(nums: number[], target: number): number[] {
+  const result: [number, number] = [-1, -1];
+
+  // Map<compliment, index>
+  const compliments = new Map<number, number>();
+
+  for (let i = 0; i < nums.length; i++) {
+    const number = nums[i];
+    const compliment = target - number;
+    if (compliments.has(compliment)) {
+      const index = compliments.get(compliment);
+      return [index === undefined ? 0 : index, i];
+    }
+    compliments.set(number, i);
+  }
+
+  return result;
+}
